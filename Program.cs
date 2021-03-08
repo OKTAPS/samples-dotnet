8fb5e16af55933693ea0d0885f4987b33c30a3cd
patluri001 <ramsai.atluri@gmail.com>
2021-03-08 10:43:41 -0500

    added all the required files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+
+namespace sample_sdk_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
